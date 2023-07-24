f302cb573f5ed941d61e9ec9295a5401a87f993f
Chiri Vulpes <6081834+ChiriVulpes@users.noreply.github.com>
2023-07-25 08:07:57 +1200

    Add release action

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,5 +1,7 @@
+import core from "@actions/core";
 import { build, Platform } from "electron-builder";
 import gulpSass from "gulp-sass";
+import fs from "mz/fs";
 import sass from "sass";
 import Electron from "./gulp/Electron";
 import Task, { Pipe, remove, Series, symlink, watch } from "./gulp/Task";
@@ -44,28 +46,58 @@ const nodeModulesCopy = new Pipe("node-modules", "script/window/node_modules/**/
 //
 
 new Task("watch", remove("out"))
-	.then(watchScriptWindow, watchScriptApp, style, statik, nodeModules)
+	.then(watchScriptWindow, watchScriptApp, style, statik, nodeModules, version)
 	.then(Electron.start("out"))
 	.then(
 		watch("style/**/*.scss", new Series(style).then(Electron.restart)),
 		watch(["static/**/*", "!static/node_modules/**/*"], new Series(statik).then(Electron.restart)))
 	.create();
 
+interface VersionObject {
+	version: string;
+	commit: string;
+}
+
+let versionObject: VersionObject | undefined;
+async function version () {
+	const FETCH_HEAD = (await fs.readFile(".git/FETCH_HEAD", "utf8"))?.slice(0, 7);
+	if (!FETCH_HEAD)
+		throw new Error("Could not find commit hash");
+
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+	const packageVersion = JSON.parse(await fs.readFile("static/package.json", "utf8") ?? "{}").version as string | undefined;
+	if (!packageVersion)
+		throw new Error("Could not find package version");
+
+	await fs.writeFile("out/version", JSON.stringify(versionObject = {
+		version: packageVersion,
+		commit: FETCH_HEAD,
+	}));
+
+	core.setOutput("version", versionObject.version);
+	core.setOutput("name", `${versionObject.version}-${versionObject.commit}`);
+}
+
 new Task("build", remove(["dist", "out"]))
-	.then(scriptWindow.compile, scriptApp.compile, style, statik, nodeModulesCopy)
+	.then(scriptWindow.compile, scriptApp.compile, style, statik, nodeModulesCopy, version)
 	.then(() => build({
-		targets: Platform.WINDOWS.createTarget(),
+		targets: new Map([Platform.WINDOWS, Platform.LINUX]
+			.flatMap(platform => [...platform.createTarget().entries()])),
 		config: {
 			appId: "chirivulpes.magictranslator",
 			productName: "MagicTranslator",
-			copyright: "Copyright © 2022 Chiri Vulpes",
+			copyright: `Copyright © ${new Date().getFullYear()} Chiri Vulpes`,
 			directories: {
 				app: "out",
 			},
 			win: {
-				target: "portable",
+				target: ["portable", "zip"],
+			},
+			linux: {
+				target: ["AppImage", "zip"],
 			},
 			includeSubNodeModules: true,
+			buildVersion: `${versionObject!.version}-${versionObject!.commit}`,
 		},
 	}))
 	.create();
